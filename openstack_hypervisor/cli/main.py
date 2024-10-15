52a26325d8512741d21030fc4d7b4d34ffdfd5b8
Guillaume Boutry <guillaume.boutry@canonical.com>
2024-10-15 18:02:02 +0200

    Add list-nic cli
    Add cli capabilities under the snap top-level name. Add a `list-nic`
    command to allow the snap returning the nics present, and which are
    candidates for being pulled in OVN/OVS.
    
    Signed-off-by: Guillaume Boutry <guillaume.boutry@canonical.com>

--- openstack_hypervisor/cli/main.py
+++ openstack_hypervisor/cli/main.py
@@ -11,3 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import click
+
+from openstack_hypervisor.cli.interfaces import list_nics
+from openstack_hypervisor.cli.log import setup_root_logging
+
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group("init", context_settings=CONTEXT_SETTINGS)
+@click.option("-v", "--verbose", is_flag=True, help="Increase output verbosity")
+def cli(verbose: bool):
+    """Set of utilities for managing the hypervisor."""
+
+
+def main():
+    """Register commands and run the CLI."""
+    setup_root_logging()
+    cli.add_command(list_nics)
+
+    cli()
+
+
+if __name__ == "__main__":
+    main()
